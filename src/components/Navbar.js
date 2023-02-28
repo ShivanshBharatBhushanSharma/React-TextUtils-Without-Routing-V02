36bd3ce5ecb522d88ab6b649b145847da9cf14e2
ShivanshBharatBhushanSharma <shivansh.sharmas1999@gmail.com>
2023-02-28 17:22:46 +0530

    My Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         {/* <Link className="navbar-brand" href="/"> {props.title} </Link>*/}
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
@@ -25,7 +25,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               {/* <Link className="nav-link active" aria-current="page" href="/"> Home </Link>*/}
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
